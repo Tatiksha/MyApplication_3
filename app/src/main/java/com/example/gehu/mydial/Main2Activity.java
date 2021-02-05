1d7b672fdfc52c3cb01b289628aaffc4ed4f70fc
Tatikshauniyal <tatikshauniyal025@gmail.com>
2021-02-05 11:44:17 +0530

    first commit

---
+++ app/src/main/java/com/example/gehu/mydial/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.example.gehu.mydial;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+TextView t;
+Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        i=getIntent();
+        t=findViewById(R.id.textView2);
+
+        t.setText(i.getStringExtra("msg1"));
+
+
+    }
+}
---
+++ app/src/main/java/com/example/gehu/mydial/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.gehu.mydial;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+    EditText n1,n2;
+    Intent i;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        i=new Intent(MainActivity.this,Main2Activity.class);
+        b=findViewById(R.id.button);
+        n1=findViewById(R.id.editText);
+        n2=findViewById(R.id.editText2);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i.putExtra("msg1",n1.getText().toString());
+                startActivity(i);
+            }
+        });
+    }
+}
